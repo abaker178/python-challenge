e11a05620ebd847ed313a33a16d44cfc087e4c85
Andrew Baker <abaker178@gmail.com>
2020-12-06 12:48:05 -0500

    added function to properly format the DOB -- no bugs

--- PyBoss/pyboss.py
+++ PyBoss/pyboss.py
@@ -12,6 +12,13 @@ def splitName(i, name):
     emp_list[i+1].append(name.split(" ")[0]) # split the Name column into two different columns
     emp_list[i+1].append(name.split(" ")[1])
 
+# split the DOB into year, month, and day, reorder them, then rejoin them with "/"
+def formatDOB(i, dob):
+    dob_list = dob.split("-")
+    dob_list.append(dob_list[0]) # add year to the end of the list...
+    dob_list.pop(0) # ... then remove it
+    emp_list[i+1].append("/".join(dob_list))
+
 # open CSV, read it, store headers, store the rest of the data, then close the file
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -22,9 +29,8 @@ for i in range(len(csvdata)):
     emp_list.append([]) # add new list(row) to the new employee list
     emp_list[i+1].append(csvdata[i][0]) # store the Emp ID unchanged
     splitName(i, csvdata[i][1]) # split the Name column into two different columns
+    formatDOB(i, csvdata[i][2]) # rewrite the DOB column to the proper format
     print(emp_list[i+1])
-
-# rewrite the DOB column to the proper format
 
 # rewrite the SSN column to the proper format
 
