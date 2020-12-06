5b15ed9f931e809a61e2cb6c72bc7ea61d221f8b
Andrew Baker <abaker178@gmail.com>
2020-12-06 16:41:06 -0500

    finished functions and printing expected results

--- PyParagraph/pyparagraph.py
+++ PyParagraph/pyparagraph.py
@@ -12,21 +12,36 @@ wordcount = 0
 def getWordCount(t):
     return len(t.split(" "))
 
-# get sentence count
+# get the total letter count
+def getLetterCount(t):
+    return len(re.findall(r"[a-zA-Z]", t))
+
+# get total sentence count
+def getSentenceCount(t):
+    return len(re.split("(?<=[.!?])[\"\']? +", t))
 
 # get average letters per word
+def getLettersPerWord(t):
+    return int(round(getLetterCount(t) / getWordCount(t), 0))
 
 # get average words per sentence
+def getWordsPerSentence(t):
+    return int(round(getWordCount(t) / getSentenceCount(t),0))
 
-# open CSV, read it, store headers, store the rest of the data, then close the file
+# open text file, store it as a list of lines, then close the file
 with open(textpath, "r") as textreader:
-    text = textreader.read()
+    textlistraw = textreader.readlines()
+
+# format text to all be on one line
+textlistraw = list(filter(lambda line: line!="\n", textlistraw)) # did a bunch of extra practice on SoloLearn. Learned about filtering and lambda functions
+textlist = [line.replace("\n", "") for line in textlistraw]
+text = " ".join(textlist)
 
 # print results
 title = "Paragraph Analysis"
 print(title)
 print("-" * len(title))
 print(f"Approximate Word Count: {getWordCount(text)}")
-#print(f"Approximate Sentence Count: {getSentCount(text)}")
-#print(f"Average Letter Count: {getLettersPerWord(text)}")
-#print(f"Average Sentence Length: {getWordsPerSentence(text)}")
+print(f"Approximate Sentence Count: {getSentenceCount(text)}")
+print(f"Average Letter Count: {getLettersPerWord(text)}")
+print(f"Average Sentence Length: {getWordsPerSentence(text)}")
