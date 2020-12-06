597b5505323561665d4aeef80579199f77b170f6
Andrew Baker <abaker178@gmail.com>
2020-12-06 14:57:38 -0500

    restructured the flow with having the results call a function for each analysis point

--- PyParagraph/pyparagraph.py
+++ PyParagraph/pyparagraph.py
@@ -5,14 +5,10 @@ import re # finally!
 # variables
 filename = input("Which text file from ../Resources/ would you like to anayze? (do not include extension) ") + ".txt"
 textpath = os.path.join("Resources",filename)
+wordcount = 0
 
 # functions
-
-# open CSV, read it, store headers, store the rest of the data, then close the file
-with open(textpath, "r") as textreader:
-    text = textreader.read()
-
-# get word count
+# get the total word count
 
 # get sentence count
 
@@ -20,4 +16,15 @@ with open(textpath, "r") as textreader:
 
 # get average words per sentence
 
+# open CSV, read it, store headers, store the rest of the data, then close the file
+with open(textpath, "r") as textreader:
+    text = textreader.read()
+
 # print results
+#title = "Paragraph Analysis"
+#print(title)
+#print("-" * len(title))
+#print(f"Approximate Word Count: {getWordCount(text)}")
+#print(f"Approximate Sentence Count: {getSentCount(text)}")
+#print(f"Average Letter Count: {getLettersPerWord(text)}")
+#print(f"Average Sentence Length: {getWordsPerSentence(text)}")
