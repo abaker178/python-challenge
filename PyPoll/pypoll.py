a3ab6b81d2ad0f602a1a88ff50e4c2480508c473
Andrew Baker <abaker178@gmail.com>
2020-12-05 19:45:10 -0500

    added total vote count and iteration to get vote counts for each candidate || need to calc and store percentages and print/export results

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -4,7 +4,6 @@ import os
 csvpath = os.path.join("Resources","election_data.csv")
 
 # declare variables
-total_votes = 0
 candidates = {
         "names": [],
         "votes": [],
@@ -16,12 +15,22 @@ with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csvheader = next(csvreader)
     csvdata = list(csvreader)
-    
-# print(csvdata) DO NOT RUN THIS CODE
+
 # store the count of the remaining rows (total votes)
+total_votes = len(csvdata)
+
+print(total_votes)
 
 # iterate through each row and count up the votes for each candidate
+for row in csvdata:
+    # if the next candidate is not currently in the list, add them and a new vote counter for them
+    if row[2] not in candidates["names"]:
+        candidates["names"].append(row[2])
+        candidates["votes"].append(0)
+    # tally the vote for the applicable candidate
+    candidates["votes"][candidates["names"].index(row[2])] += 1
 
+print(candidates)
 # iterate through each candidate and store the vote percentage they received
 
 # print results
