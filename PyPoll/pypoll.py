c96453744c9ec1873874a1f6301b2e7b920b479d
Andrew Baker <abaker178@gmail.com>
2020-12-05 19:58:27 -0500

    added percentage iterator and initial results printing || need to write winner logic and write results to a file

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -19,8 +19,6 @@ with open(csvpath, 'r') as csvfile:
 # store the count of the remaining rows (total votes)
 total_votes = len(csvdata)
 
-print(total_votes)
-
 # iterate through each row and count up the votes for each candidate
 for row in csvdata:
     # if the next candidate is not currently in the list, add them and a new vote counter for them
@@ -30,9 +28,25 @@ for row in csvdata:
     # tally the vote for the applicable candidate
     candidates["votes"][candidates["names"].index(row[2])] += 1
 
-print(candidates)
-# iterate through each candidate and store the vote percentage they received
+# iterate through each candidate and store the vote percentage they received rounded with proper formatting
+for i in range(len(candidates["names"])):
+    temp_percent = (candidates["votes"][i] / total_votes) * 100
+    temp_rounded_percent = round(temp_percent, 3)
+    candidates["percent"].append(str(temp_rounded_percent) + "%")
+
+print (candidates)
 
 # print results
+title = "Election Results"
+divider = "-" * len(title)
+print(title)
+print(divider)
+print(f'Total Votes: {total_votes}')
+print(divider)
+for i in range(len(candidates["names"])):
+    print(f'{candidates["names"][i]}: {candidates["percent"][i]} ({candidates["votes"][i]})')
+print(divider)
+print("Winner:") # need to figure out how best to do this
+print(divider)
 
 # write results to a text file
